d502ab558d799f2db43fbed03132d2d57b2696b7
Anthony <enzo.cpc@Gmail.com>
2015-08-12 01:52:53 +0800

    First octowookie checkin

---
+++ octowookie/src/main/java/com/xyzeelab/octowookie/App.java
@@ -0,0 +1,13 @@
+package com.xyzeelab.octowookie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
